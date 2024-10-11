ce196777b6c1327170d16bae9cd7a477d7654c7e
shresth19 <shresth190603@gmail.com>
2024-10-11 22:46:25 +0530

    new pattern

--- preprocessor.py
+++ preprocessor.py
@@ -2,7 +2,8 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = r'\d{1,2}/\d{1,2}/\d{2,4}\s*,\s*\d{1,2}:\d{2}\s*[apAP][mM]\s*'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s(?:[apAP][mM])?\s'
+
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
